c3d0224b34be059959c279af4f8914b59cd49962
tuniscorp <74728408+tuniscorp@users.noreply.github.com>
2021-11-05 16:33:11 -0500

    first upload of talesofthecrypto

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
             
             <div className="footer" >
                 <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-                    Cryptoverse <br />
+                    TalesOfTheCrypto <br />
                     All rights reserved
                 </Typography.Title>
                 <Space>
